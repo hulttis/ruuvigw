94a58c8638cf8523c0f56fdae0117a275b9422e9
Timo Koponen <hulttis@gmail.com>
2020-01-12 12:44:45 +0200

    4.1.1 (200111) - bleak

--- do_upgrade_ruuvigw_github.sh
+++ do_upgrade_ruuvigw_github.sh
@@ -3,7 +3,7 @@
 systemctl stop ruuvigw
 rm -frv /app/ruuvigw/new
 cp -v /app/ruuvigw/ruuvigw.json /app/ruuvigw.json.bk
-git clone --single-branch https://github.com/hulttis/ruuvigw.git /appruuvigw/new
+git clone --single-branch https://github.com/hulttis/ruuvigw.git /app/ruuvigw/new
 cp -vr /app/ruuvigw/new/* /app/ruuvigw/.
 cp -vr /app/ruuvigw.json.bk /app/ruuvigw/ruuvigw.json
 source /app/ruuvigw/env/bin/activate
